691ce41af9aebddcd38bbff82548636847980b26
James Adedini <145149678+ADEDINIJAMES@users.noreply.github.com>
2024-06-06 14:04:08 +0100

    eureka servercommit

--- src/test/java/com/tumtech/serviceregistry/ServiceRegistryApplicationTests.java
+++ src/test/java/com/tumtech/serviceregistry/ServiceRegistryApplicationTests.java
@@ -3,11 +3,15 @@ package com.tumtech.serviceregistry;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class ServiceRegistryApplicationTests {
 
     @Test
     void contextLoads() {
+        assertTrue(true);
     }
 
 }
